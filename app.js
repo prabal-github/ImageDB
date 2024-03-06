2c85119fa90cf5fdf84a01541eb97638561d58c9
prabal-github <prabal.chow09009.pc@gmail.com>
2024-03-06 12:14:31 +0530

    minor

--- app.js
+++ app.js
@@ -53,5 +53,5 @@ app.post('/images', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
